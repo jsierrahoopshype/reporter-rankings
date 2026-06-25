e6af873b7a53150a62842f36246e6d8851ebc6b5
GitHub Action <action@github.com>
2026-06-25 20:01:53 +0000

    Update reporter rankings data - 2026-06-25 20:01

--- reporter_data.js
+++ reporter_data.js
@@ -1,12 +1,12 @@
 // Reporter Rankings Data
-// Generated: 2026-06-25T14:34:51.229505
+// Generated: 2026-06-25T20:01:53.569890
 // Total Rumors: 0
 // Processed: 0
 // Total Reporters: 0
 // Total Outlets: 0
 
 const REPORTER_DATA = {
-  "generated_at": "2026-06-25T14:34:51.229505",
+  "generated_at": "2026-06-25T20:01:53.569890",
   "total_rumors": 0,
   "processed_rumors": 0,
   "total_reporters": 0,
